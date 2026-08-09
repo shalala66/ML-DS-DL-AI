7a815770442df073f83c788253bfa7ad7ac5ab65
Tural Jalilov <58109772+shalala66@users.noreply.github.com>
2026-08-10 01:15:45 +0400

    Solved quadratic-equations-exercises.py

--- Mathematics/Equation-Types/quadratic-equations-exercises.py
+++ Mathematics/Equation-Types/quadratic-equations-exercises.py
@@ -8,6 +8,14 @@
     Solve the equation x² - 5x + 6 = 0. Compute the discriminant and both roots, then print the results.
 """
 # Your Solution...
+a, b, c = 1, -5, 6
+
+D = b**2 - 4 * a * c
+
+x1 = (-b + D**0.5) / 2 * a
+x2 = (-b - D**0.5) / 2 * a
+
+print(f"x1 = {x1} and x2 = {x2}")  # x1 = 3.0 and x2 = 2.0
 
 
 """
@@ -17,4 +25,15 @@
     Print a message indicating whether the roots are real and distinct, real and equal, or complex.
 """
 # Your Solution...
+# x² + 4x + 5 = 0
+
+a, b, c = 1, 4, 5
+
+D = b**2 - 4 * a * c
+
+if D > 0: nat = "Real and distinct"
+elif D == 0: nat = "Real and equal"
+else: nat = "Complex"
+
+print(f"Nature of Roots: {nat}")   # Nature of Roots: Complex
 
